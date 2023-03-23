8e7fc03eb0288601863f1d677f40c21675af5156
Prakashkumarsk <prakashdeeban@gmail.com>
2023-03-23 12:47:55 +0530

    devloper devlop the file

--- Demojava.java
+++ Demojava.java
@@ -1,5 +1,10 @@
 package com.selenium;
 
 public class Demojava {
+	public static void main(String[] args) {
+		System.out.println("india");
+		System.out.println("india is my county");
+		System.out.println("not all american");
+	}
 
 }
